2b06e385980730c3887abcbea93e62657b15f5e9
Ninad Nirbhavane <nirbhavaneninad@gmail.com>
2021-10-13 09:09:48 +0530

    Second Commit

---
+++ Project 2- Guess Number(User)/Guess_Number.py
@@ -0,0 +1,19 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)  #returns random integer 
+    guess = 0
+    #keep guessing
+    while guess != random_number:   #base case
+        guess = int(input(f"Guess a number between 1 and {x}:  "))
+        if guess < random_number:
+            print('Sorry, guess again Too Low')
+        elif guess > random_number: 
+            print('Sorry, guess again Too High')
+
+    print(f'Yay, congrats. You have guessed the number {random_number} correctly')
+
+
+   
+
+guess (10)
